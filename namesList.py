9fbcc2f5b017a387bb7c1087b095adf16492846a
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-01-15 14:02:27 +0000

    minor changes made

---
+++ namesList.py
@@ -0,0 +1,6 @@
+'''An array is a data structure that is essentially a list with elements of the same datatype'''
+
+namesList = ["Zhandos", "Eshan", "Sami", "Loran", "Freddie"]
+for name in namesList:
+    print(name)
+
