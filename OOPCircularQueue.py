0a9fdd3379dc31c0506fa99f5d1f94f72662d6ce
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-01-16 10:10:38 +0000

    working on OOP queue

--- OOPCircularQueue.py
+++ OOPCircularQueue.py
@@ -1,2 +1,15 @@
+'''
+This code is essentially 'circularQueue.py' but using OOP. An array that acts as a circular queue of 5 elements is created
+that the user is able to add and remove from.
+'''
+
 class Circular_Queue():
-    def __init__(self,)
+   
+    def __init__(self):
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+
+    def enQueue(self, theItem):
+        if size == 5:
+            print("ERR: Queue full.")
