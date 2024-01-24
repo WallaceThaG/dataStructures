39992a462cdc7c28c6f820cdaf91a613fd89de48
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-01-24 10:43:50 +0000

    TODO: work on deleting nodes

--- Structures/linkedLists/linkedList.py
+++ Structures/linkedLists/linkedList.py
@@ -1,11 +1,13 @@
-from typing import Any
-
+'''
+Implementation of a linked list in python using OOP
+'''
 
 class Node(): # a node has data and a pointer to the next node
     
-    def __init__(self, theData):
+    def __init__(self, theData): # constructor method
         self.data = theData
-        self.next = None
+        self.next = None 
+        # linked list is empty when it is initialised so attributes set to none
 
     def setNext(self, newNext):
         self.next = newNext
@@ -19,26 +21,42 @@ class Node(): # a node has data and a pointer to the next node
 class LinkedList():
     
     def __init__(self):
-        self.head = None
+        self.head = None # Initialise to None as the linked list is empty
 
     def add(self, data):
         newNode = Node(data)
         if self.head is None:
             self.head = newNode
         else:
+            # update pointers so new node is the head
             newNode.setNext(self.head)
             self.head = newNode
 
     def traverse(self):
-        current = self.head
+        current = self.head # start at head of list
         
         while current is not None:
             print(current.getData())
             current = current.getNext()
+
+    '''def delete(self, data):
+        # start at head of list
+        current = self.head
+        # check if head node is to be deleted
+        if current.getData() == data:
+            self.head = current.getNext()
+        else:
+            while current.getNext != data:
+                current = current.getNext
+            temp = current.getNext()
+            current.setNext(temp.getNext())'''
+    # commented out method needs work
 
 people = LinkedList()
 people.add("Dipsy")
 people.add("Po")
 people.add("Laa-Laa")
 people.traverse()
+print(people.head)
 print(people) # returns a memory address
+people.traverse()
