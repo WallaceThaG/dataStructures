808488c3d388b1c19b0e40f7402e8ecd86502953
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-02-20 11:00:13 +0000

    working on oop stack

--- Structures/stacks/oopStack.py
+++ Structures/stacks/oopStack.py
@@ -1,17 +1,35 @@
 class Stack():
 
-    def __init__(self):
+    def __init__(self, theSize):
         self.top = None
+        self.size = theSize
+        self.length = 0
 
-    def pop(self):
+    def isFull(self):
+        if self.length == self.size:
+            return True
+        else:
+            return False
+    
+    def isEmpty(self):
+        if self.length == 0:
+            return True
+        else:
+            return False
         
-        
+    def push(self, data)
+
 class Node():
 
-    def __init__(self, theData, thePrev):
+    def __init__(self, theData):
         self.data = theData
-        self.previous = thePrev
+        self.next = None
 
-    def push(self, newNext):
-        self.top = newNext
-        
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setNext(self, newNext):
+        self.next = newNext
