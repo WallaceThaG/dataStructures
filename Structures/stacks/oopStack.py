70dfc49273ca69c7b6249f02563100288668646e
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-02-27 10:48:10 +0000

    oop stack working

--- Structures/stacks/oopStack.py
+++ Structures/stacks/oopStack.py
@@ -1,26 +1,50 @@
 class Stack():
 
-    def __init__(self, theSize):
+    # constructor
+    def __init__(self):
         self.top = None
-        self.size = theSize
-        self.length = 0
 
-    def isFull(self):
-        if self.length == self.size:
-            return True
+    def push(self, data):
+        newNode = Node(data)
+
+        # check if stack is not empty
+        if not self.isEmpty():
+            # point to next element in the list
+            newNode.setNext(self.top)
+        # set top to the new node
+        self.top = newNode
+
+    def pop(self):
+        poppedData = None
+        # check if empty
+        if self.isEmpty():
+            print("ERR: STACK UNDERFLOW")
         else:
-            return False
+            poppedData = self.top.getData()
+            self.top = self.top.getNext()
+
+        return poppedData
+
+    def peek(self):
+        peekedData = None
+
+        if self.isEmpty():
+            print("ERR: STACK EMPTY")
+        else:
+            peekedData = self.top.getData()
+
+        return peekedData
     
     def isEmpty(self):
-        if self.length == 0:
+        if self.top == None:
             return True
         else:
             return False
         
-    def push(self, data)
 
 class Node():
 
+    # constructor
     def __init__(self, theData):
         self.data = theData
         self.next = None
@@ -33,3 +57,22 @@ class Node():
     
     def setNext(self, newNext):
         self.next = newNext
+
+def useStack():
+    myStack = Stack()
+    print(myStack.peek())
+    myStack.push("Beans")
+    myStack.push("Greens")
+    myStack.push("Potatos")
+    myStack.push("Tomatos")
+    myStack.push("Yams")
+    myStack.push("Hams")
+
+    peekedItem = myStack.peek()
+    print(peekedItem)
+
+    for i in range(8):
+        poppedItem = myStack.pop()
+        print(poppedItem)
+
+useStack()
