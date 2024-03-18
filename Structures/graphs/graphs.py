189256da8652292b7744d1bc6aea457371b92d33
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-03-18 15:18:45 +0000

    working on different graph implementations

---
+++ Structures/graphs/graphs.py
@@ -0,0 +1,10 @@
+graphMatrix = [
+    [0,0,1,1,0,0],
+    [0,0,1,1,0,0],
+    [1,1,0,1,1,1],
+    [1,1,1,0,0,1],
+    [0,0,1,0,0,0],
+    [0,0,1,1,0,0]
+]
+
+# this represents a bidirectional, unweighted graph with 6 nodes. 1 represents a connection, 0 represents no connection.
